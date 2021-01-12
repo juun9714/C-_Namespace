d667376fac6ed2053ff1d15ff0fed4a44a14615d
june <lovekwon200@gmail.com>
2021-01-12 22:18:08 +0900

    until name

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -1,40 +1,44 @@
 #include <iostream>
-using namespace std;
 
 int n;
-void set();
+void set() {
+	n = 10;
+}
 
 namespace doodle {
-	int n;
+	
 	void set();
+
+	namespace google {
+		void set();
+		int n;
+		//이렇게 google의 n이 google의 set보다 뒤에 선언되어 있으면 참조를 자기꺼를 안하고, 
+		//자기 밖에 있는 것을 참조하게 된다.
+	}
+	int n;
 }
 
-namespace google {
-	int n;
-	void set();
-}
 
 int main() {
+	using namespace std;
 
+	using namespace doodle;
 	::set();
 	doodle::set();
 	google::set();
 
 	cout << ::n << endl;
 	cout << doodle::n << endl;
-	cout << google::n << endl;
+	cout << doodle::google::n << endl;
 	
 	return 0;
+}
+
+void doodle::google::set() {
+	n = 30;
 }
 
 void doodle::set() {
 	n = 20;
 }
 
-void set() {
-	n = 10;
-}
-
-void google::set() {
-	n = 30;
-}
