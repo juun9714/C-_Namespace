9e52e694b713bf8abf708e683d5156c440f7ae7d
june <lovekwon200@gmail.com>
2021-01-12 17:11:08 +0900

    강좌

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,20 @@
 #include  <iostream>
-#include "head.h"
+
+namespace a {
+	int n;
+}
+
+namespace b {
+	int n;
+}
 
 
 int main() {
-	bestcomp::myFunc();
-	progcomp::myFunc();
+	//이름 성 : std = 성
+	a::n = 10;
+	b::n = 20;
+	std::cout <<"Hell World "<< a::n <<' '<< b::n<< std::endl;
+	//<< shift 연산 -> 출력할 인자를 분리해주는 용도 !
+	//endl : 줄바꿈
 	return 0;
 }
