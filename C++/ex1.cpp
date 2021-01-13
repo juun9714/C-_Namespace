8559e0c96ad548a7b46a509f998c3f00cd8a94a9
june <lovekwon200@gmail.com>
2021-01-13 17:29:07 +0900

    this pointer

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -1,55 +1,27 @@
 #include <iostream>
 using namespace std;
 
-//TV를 만들어보자
-
-struct TV {
-private:
-	bool powerOn;
-	int channel;
-	int volume;
-
+class MyClass {
 public:
-	void setVolume(int vol) {
-		if (vol >= 0 && vol <= 100) {
-			volume = vol;
-			cout << "Volume은 " << vol << "입니다." << endl;
-		}
-		
-		/*
-		volume이 0~100 이외의 값이 저장되는 것을 방지 
-		근데 어떤 프로그래머가 또 lg.volume=11350 이라고 프로그램을 짜면 안돼. 
-		해당 struct안에서만(main함수에서는 접근 불가) volume변수에 접근할 수 있도록 하는 방법이 없을까? 
-		접근제어 지시자가 등장 (private, protected, public)
-		*/
+	void PrintThis() {
+		cout << "나의 주소는 " << this << endl;
 	}
-
-	void on() {
-		powerOn = true;
-		cout << "TV를 켰습니다." << endl;
+	//this pointer는 자기가 소속되어 있는 객체의 주소를 가리킨다. 
+	//struct나 class에 소속되어있는 모든 함수들은 보이지 않는 매개변수로 this라는 애를 갖고 있다. 
+	/*
+	void PrintThis(MyClass * ptr) {
+		cout << "나의 주소는 " << ptr<< endl;
 	}
-
-	void off() {
-		powerOn = false;
-		cout << "TV를 껐습니다." << endl;
-	}
-
-	void setChannel(int cnl) {
-		if (cnl >= 1 && cnl <= 100) {
-			channel = cnl;
-			cout << "Channel은 " << cnl << "입니다." << endl;
-		}
-	}
+	과 같은 것임 
+	*/
 };
-
-
-
 int main() {
-	
-	TV lg;
-	lg.on();
-	lg.setChannel(10);
-	lg.setChannel(-73);
-	lg.setVolume(50);
+	MyClass a, b;
+
+	cout << "a의 주소는 " << &a << endl;
+	cout << "b의 주소는 " << &b << endl;
+
+	a.PrintThis();
+	b.PrintThis();
 	return 0;
 }
