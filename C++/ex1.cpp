549b9d6a08423a000c7ecf1ada3f77981480a301
june <lovekwon200@gmail.com>
2021-01-14 18:12:42 +0900

    static variable

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -1,61 +1,50 @@
-/*
-static : 정적 <-> 동적
-
-<동적 멤버>
-객체에 포함이 되어 있다(?
-클래스로 찍어낸 각 객체(붕어빵)마다 팥, 슈크림이 다른 것임 (각 멤버의 값이 달라)
-
-<정적 멤버>
-붕어빵에 해당되는 멤버가 아니라, 붕어빵 틀에 해당되는 멤버들 
-*/
-
 #include <iostream>
 
 using namespace std;
 
-/*
-RGB
-0~1 사이의 값을 갖는 float
-*/
+int idCounter = 1;
+//전역의 역할을 해야하지만 class Color와 밀접한 관련이 있는 변수 -> 정적 멤버 !
 
 class Color {
 public:
-	Color() :r(0), g(0), b(0){}
-	Color(float r, float g, float b):r(r), g(g), b(b) {}
+	Color() :r(0), g(0), b(0),id(idCounter++){}
+	Color(float r, float g, float b):r(r), g(g), b(b), id(idCounter++) {}
 
 	float getR() { return r; }
 	float getG() { return g; }
 	float getB() { return b; }
+	int getId() { return id; }
 
 	static Color MixColors(Color a, Color b) {
-		/*
-		생성자를 바로 호출
-		Color result((a.getR()+b.getR())/2, (a.getG() + b.getG()) / 2, (a.getB() + b.getB()) / 2);
-		return result;
-		*/
-
-		return Color((a.getR() + b.getR()) / 2, (a.getG() + b.getG()) / 2, (a.getB() + b.getB()) / 2);
-		//위의 주석과 동일 
+		return Color((a.r + b.r) / 2, (a.g + b.g) / 2, (a.b + b.b) / 2);
+		//이 MixColors라는 멤버 메서드는 아무리 객체를 찍어내도 하나밖에 없는 것이다. 
 	}
-	//MixColors라는 함수를 class안에 넣고 싶은데, static 없이 넣으면 main에서 MixColors를 호출할 때, 소속을 밝혀줘야하는데 어느 소속인지 애매해진다. 
-	//이럴때 static을 붙여주면 main에서 호출할 때, namespace처럼 Color::MixColors로 쓸 수 있다. 
-	//또한 class 안에 멤버함수로 넣으면 class의 private 멤버에 직접 접근할 수 있다. -> getR,G,B를 안써도 됨
 
-
+	static int idCounter;
+	//객체랑은 무관하다. 생성자 안에서 초기값을 설정할 수 없음
 private:
 	float r;
 	float g;
 	float b;
+
+	int id;
 };
 
+int Color::idCounter = 1; //선언과 정의를 분리해서 해줘야 한다. 
 
+/*
+장점 
+1. 큰 프로그램의 경우 각 클래스마다 counter역할을 하는 전역변수가 엄청 많을 것이다. 그 전역변수들이 겹칠 수 있음 -> 특정 클래스와 관련된 전역변수 역할을 하는 변수 : 정적멤버변수, 함수를 활용  
+*/
 
 int main() {
-	Color blue(0, 0, 1);
 	Color red(1, 0, 0);
-	
+	Color blue(0, 0, 1);
 	Color purple = Color::MixColors(blue, red);
 
 	cout << "r=" << purple.getR() << ", g=" << purple.getG() << ", b=" << purple.getB() << endl;
+	cout << "red.getId() = " << red.getId() << endl;
+	cout << "blue.getId() = " << blue.getId() << endl;
+	cout << "purple.getId() = " << purple.getId() << endl;
 	return 0;
 }
