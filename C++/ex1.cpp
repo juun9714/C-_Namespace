b92ea26c5c4639c8971d954861d1c2d1d7ea0121
june <lovekwon200@gmail.com>
2021-01-14 20:39:07 +0900

    member method 선언, 정의 분리

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -1,52 +1,63 @@
 /*
-<const의 쓰임새>
-- 실수를 방지하기 위해 사용하는 기능
-1. 매개변수의 상수화 (for 모든 함수)
-2. 메서드의 상수화 (for 멤버 메서드)
-*/
+연산자 오버로딩 62강
+ - 벡터를 사용한다. 61강
+ - 멤버 메서드를 사용한다. 61강 
+ 
+ <벡터>
+ 실수 : float, double 등을 사용한다. 
+ 벡터 : 실수 두개를 묶어 (2, 3) 와 (-1, 7): 두 개의 벡터를 더한다면 (1, 10)이라는 벡터가 나온다. 
+ 
+ 실수 * 벡터도 가능
+ 3 * (1, 5) = (3, 15)
+
+ 벡터 * 벡터 
+ 두 가지가 있다.
+
+ 1. 내적 = dot곱 = dot으로 표현
+	(2, 3) 와 (-1, 7)를 내적으로 곱한다면, (2*-1)+(3*7)의 결과인 19가 답이 된다. 
+ 2. 외적 = 곱하기로 표현
+ 
+
+ <멤버 메서드>
+ 멤버 메서드의 선언과 정의를 분리하기
+ 멤버 함수의 경우는 대부분 선언과 정의를 분리한다. 
+ 왜냐면 내 위에서 선언된 함수만 내 함수 내에서 사용할 수 있기 때문에 소스코드 맨 위에서 선언을 모두 해놓고, 
+ 정의를 아래서 따로 해야 참조할 수 있다. 
+
+ 클래스는 때때로 namespace와 같게 작동한다. 
+ --> 클래스 내의 멤버 메서드의 정의를 따로 뺄때도 namespace와 동일하게 가능?
+
+ Vector2() : x(0), y(0) {}
+	Vector2(float x, float y) : x(x),y(y){}
+
+	float GetX() const { return x; }
+	float GetY() const { return y; }
+ */
 
 #include <iostream>
 using namespace std;
 
-class Account {
+class Vector2 {
 public:
-	Account():money(0){}
-	Account(int money):money(money){}
+	Vector2();
+	Vector2(float x, float y);
 
-	void Deposit(const int d) {
-		//이 함수 안에서 매개변수인 d의 값이 변하는 경우(실수)를 방지 -> 매개변수의 상수화 기능
-		//d = money;
-		money += d;
-		//예금
-		cout << d << "원을 예금했다." << endl;
-	}
-
-	void Draw(const int d) {
-		if (money >= d) {
-			money -= d;
-			cout << d << "원을 인출했다." << endl;
-		}
-	}
-
-	int viewMoney() const{
-		//이 메서드의 경우 멤버변수를 단순히 반환하는 역할이고, 멤버 변수를 조작하지는 않는다. -> const 메서드로 만들어버리면 멤버변수(money) 조작시 에러가 뜬다. 
-		//money++;
-		return money;
-	}
-
-	//const int viewMoney() :viewMoney라는 함수의 리턴값이 const가 된다. 사실 return 값을 조작하는 일은 거의 없기 때문에 의미가 없긴 함.
-
+	float GetX() const;
+	float GetY() const;
 private:
-	int money;
+	float x;
+	float y;
+	//이런 클래스의 경우 값을 변경하기보단, 그냥 해당 값을 갖는 새로운 객체 하나를 생성해버리는게 낫다. -> set함수 안만들거임
 };
 
 
 
 int main() {
-	Account june(200);
-	june.Deposit(100);
-	june.Draw(20);
-	cout << june.viewMoney() <<"원이 잔액이다."<< endl;
 	return 0;
-
 }
+
+
+Vector2::Vector2() : x(0), y(0) {}
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+float Vector2::GetX() const { return x; }
+float Vector2::GetY() const { return y; }
