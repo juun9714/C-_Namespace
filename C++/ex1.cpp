bc7f7f22a185466c2f95fed51f5aec9c26258882
june <lovekwon200@gmail.com>
2021-02-01 16:06:39 +0900

    상속

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -2,73 +2,68 @@
 #include <iostream>
 using namespace std;
 
-class String {
+class Base {
 public:
-	String() {
-		cout << "String() 생성자 호출" << endl;
-		strData = NULL;
-		len = 0;
+	void bFunction() {
+		cout << "Hello !" << endl;
 	}
-
-	String(const char* str) {
-		cout << "String(const char* str) 생성자 호출" << endl;
-		len = strlen(str);
-		strData = new char[len+1];//NULL을 위한 +1
-		cout << "strData 할당 : "<<(void *)strData<< endl;
-		strcpy(strData, str);//깊은 복사
-		//iostream에 포함되어 있음
-	}
-
-	String(const String& rhs) {
-		//복사생성자의 경우 class의 정의안에서 해당 class를 매개변수로 쓰는것 불가(String(String rhs)), 
-		//다른 생성자나 멤버 메서드에서는 가능
-		cout << "String(String& rhs) 생성자 호출" << endl;
-
-		strData = new char[rhs.len + 1];//NULL을 위한 +1
-		cout << "strData 할당 : " << (void*)strData << endl;
-		strcpy(strData, rhs.strData);//깊은 복사
-		len = rhs.len;
-	}
-
-	~String(){
-		//메모리 해제
-		cout << "~String() 소멸자 호출" << endl;
-		delete[] strData;
-		cout << "strData 해제됨 : " <<(void*)strData<< endl;
-		strData = NULL;
-		//해제된 메모리에 접근을 막기 위한 NULL 할당
-	}
-
-	char* GetStrData() const{
-		return strData;
-	}
-
-	int GetLen() const{
-		return len;
-	}
-
-private:
-	char* strData;//동적할당한 문자열의 주소를 저장?
-	int len;
+	
+	int bnum;
 };
 
+class Derived : public Base {
+public:
+	void dFunction() {
+		cout << "Hello ?" << endl;
+	}
+
+	int dnum;
+};
 
 int main() {
-	String s1("안녕");
-	String s2(s1);//복사 생성자 
-	String s3;
-
+	Base b;
+	Derived d;
 	/*
-	복사 생성자
-	: 선언되는 객체와 같은 자료형의 객체를 인수로 전달하는 생성자
-	: 인수는 참조자(생성자 정의시 &)로 전달
-	: 전달되는 인수는 대개 const 선언
-	-> default로 정의되는 복사생성자가 있지만, 포인터 복사의 경우 얕은 복사가 이루어지게 됨. 
-	-> 그렇기 때문에 포인터복사를 해야하는 경우 복사 생성자를 선언, 정의해주어야 한다. 
+	b.bFunction();
+	b.bnum = 1;
+
+	d.bFunction();
+	d.bnum = 2;
+	d.dFunction();
+	d.dnum = 1;
 	*/
-
-	cout << s1.GetStrData() << endl;
-	cout << s2.GetStrData() << endl;
-
 	return 0;
 }
+
+
+/*
+상속
+부모가 자식에게 물려주는 것 
+
+부모가 가진 것 -> 멤버 메서드, 멤버 변수 
+자식에게 부모가 가진 멤버를 똑같이 전수를 해주는 것임 
+
+자식은 또 자기만의 고유한 멤버를 가질 수 있음
+
+자식 클래스가 부모클래스에 포함이 되는 것임
+
+왜 ? 
+-> 남자와 총각의 관계를 생각해보면 쉽다. 
+
+*/
+
+/*
+접근제어
+1. private 
+: 외부 접근 불가
+: 자식 클래스도 접근 불가, 상속은 가능함
+2. public 
+: 외부 접근 가능
+: 자식 클래스의 접근 가능
+3. protected
+: 외부 접근 불가 
+: 자식 클래스에서 접근 가능
+: 외부말고 자기 자식들에게만 접근 허용 
+
+
+*/
