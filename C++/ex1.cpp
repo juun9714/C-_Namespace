138717a9cf61d5701e9d7308aba659010fe80d63
june <lovekwon200@gmail.com>
2021-02-03 18:04:15 +0900

    encapsulate

--- C++/ex1.cpp
+++ C++/ex1.cpp
@@ -2,77 +2,56 @@
 #include <iostream>
 using namespace std;
 
-class Base {
-public:
-	int bPublic;
-
-protected:
-	int bProtected;
-	//protected는 부모가 자식한테 물려줄 멤버를 지정하는 것이기 때문에 부모에서만 설정해주면 된다.
+class Point {
 private:
-	int bPrivate;
-};
-/*
-상속시 쓰는 접근제어의 의미
--> 지정한 지시자보다 더 넓은 공개범위를 갖고 있는 멤버를 지정된 지시자 수준으로 내린다. 
-*/
-class Derived : protected Base {
+	int xpos, ypos;
 public:
-	int dPublic;
-	void DFunc() {
-		bPublic = 1;
-		bProtected = 2;
-		//bPrivate = 3;
-		dPublic = 4;
-		dPrivate = 5;
-
+	void Init(int x, int y) {
+		xpos = x;
+		ypos = y;
 	}
+	void ShowPointInfo() const {
+		cout << "[" << xpos << ", " << ypos << "]" << endl;
+	}
+};
+
+class Circle {
 private:
-	int dPrivate;
+	Point center;
+	int radius;
+public:
+	void Init(int x,int y, int radius_) {
+		center.Init(x, y);
+		radius = radius_;
+	}
+	void ShowCircleInfo() const {
+		cout << "radius: " << radius << endl;
+		center.ShowPointInfo();
+	}
+};
+
+class Ring {
+private:
+	Circle inner;
+	Circle outer;
+public:
+	void Init(int in_x, int in_y, int in_ra, int out_x, int out_y, int out_ra) {
+		inner.Init(in_x, in_y, in_ra);
+		outer.Init(out_x, out_y, out_ra);
+	}
+
+	void ShowRingInfo() {
+		cout << "Inner Circle Info..." << endl;
+		inner.ShowCircleInfo();
+		cout << "Outer Circle Info..." << endl;
+		outer.ShowCircleInfo();
+	}
 };
 
 int main() {
-	Base b;
-	Derived d;
+	Ring ring;
+	ring.Init(1, 1, 4, 2, 2, 9);
+	ring.ShowRingInfo();
 	
-	b.bPublic = 1;
-
-	d.bPublic = 3;
-	d.dPublic = 2;
 	return 0;
 }
-
-
-/*
-상속
-부모가 자식에게 물려주는 것 
-
-부모가 가진 것 -> 멤버 메서드, 멤버 변수 
-자식에게 부모가 가진 멤버를 똑같이 전수를 해주는 것임 
-
-자식은 또 자기만의 고유한 멤버를 가질 수 있음
-
-자식 클래스가 부모클래스에 포함이 되는 것임
-
-왜 ? 
--> 남자와 총각의 관계를 생각해보면 쉽다. 
-
-
-부모클래스에서 public으로 정의되어 있는 멤버는 자식 클래스에서도 public으로 인식
-*/
-
-/*
-접근제어
-1. private 
-: 외부 접근 불가
-: 자식 클래스도 접근 불가, 상속은 가능함
-2. public 
-: 외부 접근 가능
-: 자식 클래스의 접근 가능
-3. protected
-: 외부 접근 불가 
-: 자식 클래스에서 접근 가능
-: 외부말고 자기 자식들에게만 접근 허용 
-
-
-*/
